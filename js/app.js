6705134a520949eafabd89ab9543de1dbc8935b3
sketchman2017 <sketchman2017@gmail.com>
2016-09-28 21:57:59 +0300

    Added input for adding elements to the list. To the each list element added delete button for removing this element from the list.

---
+++ js/app.js
@@ -0,0 +1,44 @@
+angular.module("myApp",[]).run(
+function($rootScope) {}).controller("ListController", 
+  function($scope, $rootScope) {
+    $scope.get_list = function() {
+        var keys = Object.keys(localStorage);
+        var points_list = {};
+
+        for (var i = 0; i < keys.length; i++) {
+            if (keys[i].startsWith("point")) {
+                var value = localStorage.getItem(keys[i]);
+                points_list[keys[i]] = value;
+            }
+        }
+        return points_list;
+    }
+
+    $scope.put_to_list = function() {
+        var new_key = "point" + $scope.point_index;
+        localStorage.setItem(new_key, $scope.newPoint);
+        $scope.point_index++;
+    }
+
+    $scope.delete_from_list = function(current_point_index) {
+        var key_for_remove = current_point_index;
+        localStorage.removeItem(key_for_remove);
+        $scope.points = $scope.get_list();
+    }
+
+    $scope.newPoint = "";
+    $scope.points = $scope.get_list();
+    $scope.point_index = 0;
+
+    document.getElementById("new_point").addEventListener("keyup",
+        function(event) {
+            if (event.which == 13 && $scope.newPoint != "") {
+                $scope.put_to_list();
+                $scope.points = $scope.get_list();
+                $scope.newPoint = "";
+                $scope.$digest();
+            }
+        },
+        false
+    );
+});
