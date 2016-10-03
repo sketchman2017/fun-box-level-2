3df2a7b17f14b4f621bc25d757450c18f7f94f2d
sketchman2017 <sketchman2017@gmail.com>
2016-10-03 19:57:04 +0300

    Improving tests. Sequentially adding checking and removing 2 points.

--- tests/e2e.js
+++ tests/e2e.js
@@ -1,12 +1,10 @@
 describe('my app', function() {
-    it('should have a title and have 1 point after adding, checking text, deleting the point', function() {
+    it('should have a title and have 1 point after adding; checking text; deleting the point', function() {
         browser.get("http://127.0.0.1:8000/");
         expect(browser.getTitle()).toEqual('Редактор маршрутов');
 
         element(by.model('newPoint')).sendKeys(1);
         element(by.model('newPoint')).sendKeys(protractor.Key.ENTER);
-
-//        element(by.id('gobutton')).click();
 
         expect(element.all(by.repeater('point in points')).count()).toEqual(1);
 
@@ -16,7 +14,7 @@ describe('my app', function() {
         expect(element.all(by.repeater('point in points')).count()).toEqual(0);
     });
 
-    it('have 2 points after adding', function() {
+    it('have 2 points after adding; check text; deleting', function() {
         browser.get("http://127.0.0.1:8000/");
 
         element(by.model('newPoint')).sendKeys(1);
@@ -26,6 +24,19 @@ describe('my app', function() {
         element(by.model('newPoint')).sendKeys(protractor.Key.ENTER);
 
         expect(element.all(by.repeater('point in points')).count()).toEqual(2);
+
+        expect(element(by.css('li.list_element:first-of-type')).getText()).toEqual('1');
+        expect(element(by.css('li.list_element:nth-of-type(2)')).getText()).toEqual('11');
+
+        element(by.css('li.list_element:nth-of-type(2) > span > img.delete_point')).click()
+        expect(element.all(by.repeater('point in points')).count()).toEqual(1);
+
+        expect(element.all(by.repeater('point in points')).count()).toEqual(1);
+        expect(element(by.css('li.list_element:first-of-type')).getText()).toEqual('1');
+
+        element(by.css('li.list_element:nth-of-type(1) > span > img.delete_point')).click()
+        expect(element.all(by.repeater('point in points')).count()).toEqual(0);
+        
     });
 
 
