61d4635e5dcecf5d09da235d08e391c0b7252246
sketchman2017 <sketchman2017@gmail.com>
2016-10-03 19:14:10 +0300

    Title added to template. e2e test added (not all).

---
+++ tests/e2e.js
@@ -0,0 +1,29 @@
+describe('my app', function() {
+    it('should have a title and have 1 point after adding, checking text', function() {
+        browser.get("http://127.0.0.1:8000/");
+        expect(browser.getTitle()).toEqual('Редактор маршрутов');
+
+        element(by.model('newPoint')).sendKeys(1);
+        element(by.model('newPoint')).sendKeys(protractor.Key.ENTER);
+
+//        element(by.id('gobutton')).click();
+
+        expect(element.all(by.repeater('point in points')).count()).toEqual(1);
+
+        expect(element(by.css('li.list_element:first-of-type')).getText()).toEqual('1');
+    });
+
+    it('have 2 points after adding', function() {
+        browser.get("http://127.0.0.1:8000/");
+
+        element(by.model('newPoint')).sendKeys(1);
+        element(by.model('newPoint')).sendKeys(protractor.Key.ENTER);
+
+        element(by.model('newPoint')).sendKeys(11);
+        element(by.model('newPoint')).sendKeys(protractor.Key.ENTER);
+
+        expect(element.all(by.repeater('point in points')).count()).toEqual(2);
+    });
+
+
+});
